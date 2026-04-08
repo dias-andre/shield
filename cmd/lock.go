4aee8af78a5f2bcd838f8ad019a6362210fc7843
https-dre <diaso.andre@outlook.com>
2026-04-08 12:08:43 -0300

    feat(keysystem): add 'lock' command for Linux

---
+++ cmd/lock.go
@@ -0,0 +1,35 @@
+//go:build linux
+
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/dias-andre/shield/internal/core/ports"
+	"github.com/fatih/color"
+	"github.com/spf13/cobra"
+)
+
+var lockCmd = &cobra.Command{
+	Use: "lock",
+	Short: "Lock shield",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		lockable, ok := keysystem.(ports.Lockable)
+
+		if !ok {
+			return fmt.Errorf("the current keysystem adapter does not support locking")
+		}
+
+		err := lockable.Lock()
+		if err != nil {
+			return fmt.Errorf("failed to lock shield: %w", err)
+		}
+
+		color.HiGreen("🔒 Shield locked successfully!")
+		return nil
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(lockCmd)
+}
