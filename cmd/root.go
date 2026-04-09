185cc1a746ccec476ef425044ee6065f4c3abb31
https-dre <diaso.andre@outlook.com>
2026-04-09 17:23:08 -0300

    chore: add license header

--- cmd/root.go
+++ cmd/root.go
@@ -1,3 +1,10 @@
+// Copyright (C) 2026 André de Oliveira Dias (diaso.andre@outlook.com)
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
 package cmd
 
 import (
